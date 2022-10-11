44cac7346e142aeb4364ae6fa4be5946ae9e626e
fzappala <115554161+fzappala@users.noreply.github.com>
2022-10-11 16:38:05 +0200

    Update chart.dart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -31,7 +31,7 @@ class Chart extends StatelessWidget {
       return sum + (item['amount'] as double);
     });
   }
-//huhuhiuhoojhjhkjsdcl sdkchdshhwdfsdcsdfcgxd
+//works?
   @override
   Widget build(BuildContext context) {
     print(groupedTransactionValues);
